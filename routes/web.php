e3a802f920c67f1bf36ccaed690bbf25f45e9be5
khoudia96gueye <gueyekhoudialivediakhou@gmail.com>
2023-12-01 12:37:49 +0000

    authentification breeeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
-
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,13 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/', function () {
-    return view('welcomesa');
-});*/
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('', [MarketController::class, 'index2']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/accueil', [MarketController::class, 'index']);
-Route::get('/drivesasamarketplace', [MarketController::class, 'index1']);
-Route::get('/login', [MarketController::class, 'index3']);
-Route::get('/servicepage', [MarketController::class, 'index4']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
